0092682b64426d2065b297009edcbfb5a8c4e1a1
Jader Martins <jadermcs94@gmail.com>
2022-01-18 14:22:34 -0300

    fix forum train

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,5 @@
 import os
+import json
 import glob
 import tqdm
 import random
@@ -7,27 +8,33 @@ import pandas as pd
 
 path = "clear_threads/"
 files = glob.glob(path+"*_*.tsv")
-random.seed(42)
-random.shuffle(files)
+ids = json.load(open("category_ids.json"))
+names = json.load(open("categories.json"))
 
 frames = []
 counter = 0
+fsize = len(files)
 
 for filein in tqdm.tqdm(files):
+    tid = filein.split('/')[-1].split('_')[0]
     counter += 1
     df = pd.read_csv(filein, delimiter='\t', quoting=3, header=None,
                      names=["timestamp", "id", "text"])
     df.drop(columns=['timestamp'], inplace=True)
     df['reply'] = df['text'].shift(-1)
     df = df.iloc[:-1]
-    df.drop(df[(df.text.str.len() < 3) | (df.reply.str.len() < 3)].index,
-            inplace=True)
-    df.drop(df[df.text.str.contains(r'[^0-9a-zA-Z\.\,\?\!]') |
-            df.reply.str.contains(r'[^0-9a-zA-Z\.\,\?\!]')].index,
-            inplace=True)
+    namethr, namesub = ids[tid]
+    if namethr == "36":
+        continue
+    df['topic'] = names[namethr]["name"].lower()+" "+names[namethr]["subs"][namesub].lower()
+    try:
+        df.drop(df[(df.text.str.len() < 3) | (df.reply.str.len() < 3)].index,
+                inplace=True)
+    except:
+        print("Can't process", filein)
     os.remove(filein)
     frames.append(df)
-    if counter % 1000 == 0:
+    if counter % 1000 == 0 or counter == fsize:
         out = pd.concat(frames, axis=0, ignore_index=True)
         out.dropna(axis=0, inplace=True)
         frames = []
--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import tqdm
 import glob
 import pandas as pd
 import torch
+from itertools import chain
 from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
@@ -12,7 +13,7 @@ from datasets import load_dataset, load_metric
 
 files = "clear_threads/*.parquet"
 checkpoint = "models/pratofeito"
-BLOCK_SIZE = 128
+block_size = 128
 
 tokenizer = GPT2Tokenizer.from_pretrained(checkpoint)
 model = GPT2LMHeadModel.from_pretrained(checkpoint)
@@ -41,32 +42,53 @@ def prefix_function(examples):
     examples["text"] =  "<sos_u> "+examples["text"]+" <eos_u>"
     examples["topic"] = "<sos_b> "+examples["topic"]+" <eos_b>"
     examples["reply"] = "<sos_r> "+examples["reply"]+" <eos_r>"
+    examples["encoded"] = examples["text"]+examples["topic"]+examples["reply"]
     return examples
 
-datasets = datasets.map(prefix_function)
+column_names = datasets["train"].column_names
+
+datasets = datasets.map(
+    prefix_function,
+    remove_columns=column_names,
+)
 
 def tokenizer_function(examples):
-    return tokenizer(examples["text"]+examples["topic"]+examples["reply"])
+    return tokenizer(examples["encoded"], truncation=True, padding="max_length",
+                     max_length=block_size)
 
-datasets = datasets.map(tokenizer_function)
+column_names = datasets["train"].column_names
+
+datasets = datasets.map(
+    tokenizer_function,
+    batched=True,
+    num_proc=4,
+    remove_columns=column_names,
+)
+
+column_names = datasets["train"].column_names
+print(column_names)
 
 def group_texts(examples):
-    concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
+    concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
     total_length = len(concatenated_examples[list(examples.keys())[0]])
-    total_length = (total_length // BLOCK_SIZE) * BLOCK_SIZE
+    if total_length >= block_size:
+        total_length = (total_length // block_size) * block_size
     result = {
-        k: [t[i : i + BLOCK_SIZE] for i in range(0, total_length, BLOCK_SIZE)]
-        for k, t in concatenated_examples.items() }
+        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+        for k, t in concatenated_examples.items()
+    }
     result["labels"] = result["input_ids"].copy()
     return result
 
 lm_datasets = datasets.map(
     group_texts,
     batched=True,
-    num_proc=4)
+    num_proc=4,
+)
 
 training_args = TrainingArguments(
-    "test-clm",
+    "models/forums-clm",
+    run_name="forums-clm",
     evaluation_strategy="epoch",
     per_device_train_batch_size=8,
     learning_rate=2e-5,
